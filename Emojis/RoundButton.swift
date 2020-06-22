76859c600f90fa0b6c684bf6e71e2f305a21cead
Laura <laura@MacBook-Pro-de-Laura.local>
2020-06-22 13:53:28 +0200

    first commit

---
+++ Emojis/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  Emojis
+//
+//  Created by Laura Garcia Cano on 10/06/2020.
+//  Copyright © 2020 LauDecked Devs. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
