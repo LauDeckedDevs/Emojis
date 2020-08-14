7510116035784d388ea93c5a9976bdf4fd377f01
LauDecked <60990368+LauDeckedDevs@users.noreply.github.com>
2020-08-14 21:39:42 +0200

    Card view safe (#7)
    * new cardView (hardcoded)
    
    * Linking ViewController
    
    * cardview?
    
    * project organization

--- Emojis/Controller/ViewController.swift
+++ Emojis/Controller/ViewController.swift
@@ -13,11 +13,14 @@ class ViewController: UIViewController {
     //MARK: - Properties
     
     typealias Emojis = [String: String]
-    var emojiDefs: Emojis = ["🥺": "Morritos", "😍": "In love", "😘": "Besito", "💻": "Quiero mi mac",
-                             "🥵": "Horny", "🥳": "fiesta", "😝": "Lengua fuera", "😳": "vergüenza"]
+    var emojiDefs: Emojis = ["🥺": "Morritos", "😍": "In love", "😘": "Besito", "💻": "Quiero mi mac", "🥵": "Horny", "🥳": "fiesta", "😝": "Lengua fuera", "😳": "vergüenza"]
     
     @IBOutlet var buttons: [CustomButton]!
+    @IBOutlet weak var emoji: UILabel!
+    @IBOutlet weak var definition: UILabel!
+    @IBOutlet weak var similarEmojis: UILabel!
     private var usedEmojis: [String] = []
+    
     
     //MARK: - View
     
