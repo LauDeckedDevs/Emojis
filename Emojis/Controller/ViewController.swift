e896a1296021b69f2e5bb1d88d14a5227ed1e9aa
Laura Garcia <laudeckeddevs@gmail.com>
2022-03-27 21:29:57 +0200

    fixing functionality

--- Emojis/Controller/ViewController.swift
+++ Emojis/Controller/ViewController.swift
@@ -91,8 +91,4 @@ class ViewController: UIViewController {
          )
         present(alertController, animated: true, completion: nil)
     }
-    
-   /* func meow(maullido: String) {
-        print(maullido)
-    } */
 }
